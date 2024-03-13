925f9d673d8563d84d5aa3bdb8c1f72d1bf07ea7
aftar51 <afftardarda@gmail.com>
2024-03-13 13:54:08 +0700

    siiiiiiiiiiiiiiiiiiii

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Profile extends Model
 {
@@ -14,4 +15,16 @@ class Profile extends Model
         'first_name',
         'image'
     ];
+
+    // relasi to user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    // Accessor Image profile
+    public function Image() : Attribute{
+        return Attribute::make(
+            get: fn($value) => asset('/storage/profile' . $value)
+        );
+    }
 }
